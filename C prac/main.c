92dec8d9db8ad96b1d4866023ce77691433490b1
MacBookPro <tekeezy@gmail.com>
2018-07-01 20:44:22 +0900

    Initial Commit

---
+++ C prac/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C prac
+//
+//  Created by MacBookPro on 2018. 7. 1..
+//  Copyright © 2018년 MacBookPro. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
